59010f98207fe6510a88e86f0f342020a2c2a5ee
Jordan Rojas <i24050391@monclova.tecnm.mx>
2025-05-15 20:42:06 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace _07_05_25
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombreCompleto = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lstLista = new System.Windows.Forms.ListView();
+            this.lblLectura = new System.Windows.Forms.Label();
+            this.txtLectura = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNombreCompleto
+            // 
+            this.lblNombreCompleto.AutoSize = true;
+            this.lblNombreCompleto.Location = new System.Drawing.Point(29, 19);
+            this.lblNombreCompleto.Name = "lblNombreCompleto";
+            this.lblNombreCompleto.Size = new System.Drawing.Size(137, 20);
+            this.lblNombreCompleto.TabIndex = 0;
+            this.lblNombreCompleto.Text = "Nombre Completo";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(33, 42);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(264, 26);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(312, 34);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(123, 43);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // lstLista
+            // 
+            this.lstLista.HideSelection = false;
+            this.lstLista.Location = new System.Drawing.Point(33, 83);
+            this.lstLista.Name = "lstLista";
+            this.lstLista.Size = new System.Drawing.Size(402, 292);
+            this.lstLista.TabIndex = 3;
+            this.lstLista.UseCompatibleStateImageBehavior = false;
+            this.lstLista.DoubleClick += new System.EventHandler(this.lstLista_DoubleClick);
+            // 
+            // lblLectura
+            // 
+            this.lblLectura.Location = new System.Drawing.Point(748, 80);
+            this.lblLectura.Name = "lblLectura";
+            this.lblLectura.Size = new System.Drawing.Size(278, 292);
+            this.lblLectura.TabIndex = 4;
+            this.lblLectura.Text = "label1";
+            // 
+            // txtLectura
+            // 
+            this.txtLectura.Location = new System.Drawing.Point(450, 80);
+            this.txtLectura.Multiline = true;
+            this.txtLectura.Name = "txtLectura";
+            this.txtLectura.Size = new System.Drawing.Size(274, 292);
+            this.txtLectura.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1038, 450);
+            this.Controls.Add(this.txtLectura);
+            this.Controls.Add(this.lblLectura);
+            this.Controls.Add(this.lstLista);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombreCompleto);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombreCompleto;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ListView lstLista;
+        private System.Windows.Forms.Label lblLectura;
+        private System.Windows.Forms.TextBox txtLectura;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _07_05_25
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        { 
+            lstLista.Items.Insert(0, txtNombre.Text);
+            txtNombre.Clear();
+            txtNombre.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lstLista.View = View.Details;
+            lstLista.LabelEdit = true;
+            lstLista.FullRowSelect = true;
+            lstLista.GridLines = true;
+            lstLista.Columns.Add("Nombre", -2, HorizontalAlignment.Left);
+            string ruta = AppDomain.CurrentDomain.BaseDirectory;
+            DirectoryInfo dir = new DirectoryInfo(ruta);
+            foreach (var fil in dir.GetFiles())
+            {
+                lstLista.Items.Add(fil.Name);
+            }
+            foreach (var fil in dir.GetFiles("*.txt"))
+            {
+                MessageBox.Show(fil.Name);
+            }
+            foreach (var fil in dir.GetFiles("h?.*"))
+            {
+                MessageBox.Show(fil.Name);
+            }
+            foreach (var fil in dir.GetDirectories())
+            {
+                MessageBox.Show(fil.Name);
+            }
+        }
+
+        private void lstLista_DoubleClick(object sender, EventArgs e)
+        {
+            lblLectura.Text = lstLista.SelectedItems[0].Text;
+            txtLectura.Clear();
+            string lectura = "";
+            StreamReader fichero;
+            fichero = File.OpenText(lstLista.SelectedItems[0].Text);
+            while (!fichero.EndOfStream)
+            {
+                lectura = fichero.ReadLine();
+                txtLectura.Text += lectura+ "\r \n";
+                lblLectura.Text += lectura + "\r \n";
+            }
+            fichero.Close();
+        }
+    }
+}
